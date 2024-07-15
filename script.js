b018da20607df80b5c2be27375f4ada0b327ed36
LajithFrancis <lajithf@gmail.com>
2024-07-15 08:16:13 +0530

    "Updated script.js to reassign slides variable and adjust scrollLeft values; added min-width to .card in styles.css and changed min-width of an unspecified element from 350px to 300px."

--- script.js
+++ script.js
@@ -6,10 +6,13 @@ slider.addEventListener('scroll', () => {
   if (slider.scrollLeft + slider.offsetWidth >= slider.scrollWidth) {
     // Move the first slide to the end when reaching the end of the slider
     slider.appendChild(slides[0]);
+    slides = slider.children;
     slider.scrollLeft = slider.scrollLeft - slides[0].offsetWidth;
   } else if (slider.scrollLeft <= 0) {
     // Move the last slide to the beginning when reaching the beginning of the slider
     slider.insertBefore(slides[slides.length - 1], slides[0]);
-    slider.scrollLeft = slider.offsetWidth;
+    slides = slider.children;
+    slider.scrollLeft =
+      slider.scrollLeft + slides[slides.length - 1].offsetWidth;
   }
 });
